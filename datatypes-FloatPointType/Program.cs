dc6e39c0077b646b0b5117211001a4471b16b387
To Be Or Not to be <seniory@windowslive.com>
2018-04-13 02:47:48 +0300

    Add project files.

---
+++ datatypes-FloatPointType/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace datatypes_FloatPointType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float flMin = float.MinValue;
+            float flMax = float.MaxValue;
+
+            Console.WriteLine("Min Value = {0} Max Value = {1}", flMin, flMax);
+
+            float flVar = 1F / 3;
+            double dlVar = 1D / 3;
+            decimal dcVar = 1M / 3;
+
+            Console.WriteLine("\nFloat Value ={0}\nDouble Value = {1}\nDeciaml Value = {2}", flVar, dlVar, dcVar);
+            Console.ReadKey();
+
+
+        }
+    }
+}
